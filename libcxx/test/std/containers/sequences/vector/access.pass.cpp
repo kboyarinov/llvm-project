7cd840833bc1efea82cd6e17780f0500af411758
Boyarinov, Konstantin <konstantin.boyarinov@intel.com>
2021-10-18 18:51:06 +0300

    Rewrite vector access tests

--- libcxx/test/std/containers/sequences/vector/access.pass.cpp
+++ libcxx/test/std/containers/sequences/vector/access.pass.cpp
@@ -27,94 +27,109 @@
 #include "min_allocator.h"
 #include "test_macros.h"
 
-template <class C>
+struct EqComparable {
+    int value;
+
+    EqComparable(int v) : value(v) {}
+
+    friend bool operator==(const EqComparable& lhs, const EqComparable& rhs) {
+        return lhs.value == rhs.value;
+    }
+};
+
+template <typename C>
 C
 make(int size, int start = 0)
 {
     C c;
+    typedef typename C::value_type value_type;
     for (int i = 0; i < size; ++i)
-        c.push_back(start + i);
+        c.push_back(value_type(start + i));
     return c;
+}
+
+template <typename Vector>
+void test_get_basic(Vector& c, int N = 0) {
+    typedef typename Vector::value_type value_type;
+
+    for (int i = 0; i < 10; ++i)
+        assert(c[i] == value_type(N + i));
+    for (int i = 0; i < 10; ++i)
+        assert(c.at(i) == value_type(N + i));
+
+#ifndef TEST_HAS_NO_EXCEPTIONS
+    try {
+        c.at(10);
+        assert(false);
+    } catch (std::out_of_range&) {}
+#endif
+
+    assert(c.front() == value_type(N));
+    assert(c.back() == value_type(N + 9));
+}
+
+template <typename Vector>
+void test_get(int N = 0) {
+    Vector c = make<Vector>(10, N);
+    const Vector& cc = c;
+    test_get_basic(c, N);
+    test_get_basic(cc, N);
+}
+
+template <typename Vector>
+void test_set(int N = 0) {
+    typedef typename Vector::value_type value_type;
+    Vector c = make<Vector>(10, N);
+
+    for (int i = 0; i < 10; ++i) {
+        assert(c[i] == value_type(N + i));
+        c[i] = value_type(N + i + 1);
+        assert(c[i] == value_type(N + i + 1));
+    }
+    for (int i = 0; i < 10; ++i) {
+        assert(c.at(i) == value_type(N + i + 1));
+        c.at(i) = value_type(N + i + 2);
+        assert(c.at(i) == value_type(N + i + 2));
+    }
+
+    assert(c.front() == value_type(N + 2));
+    c.front() = value_type(N + 3);
+    assert(c.front() == value_type(N + 3));
+
+    assert(c.back() == value_type(N + 9 + 2));
+    c.back() = value_type(N + 9 + 3);
+    assert(c.back() == value_type(N + 9 + 3));
+}
+
+template <typename Vector>
+void test() {
+    test_get<Vector>();
+    test_set<Vector>();
+
+    test_get<Vector>(36);
+    test_set<Vector>(36);
+
+    Vector c;
+    const Vector& cc = c;
+    ASSERT_SAME_TYPE(typename Vector::reference, decltype(c[0]));
+    ASSERT_SAME_TYPE(typename Vector::const_reference, decltype(cc[0]));
+
+    ASSERT_SAME_TYPE(typename Vector::reference, decltype(c.at(0)));
+    ASSERT_SAME_TYPE(typename Vector::const_reference, decltype(cc.at(0)));
+
+    ASSERT_SAME_TYPE(typename Vector::reference, decltype(c.front()));
+    ASSERT_SAME_TYPE(typename Vector::const_reference, decltype(cc.front()));
+
+    ASSERT_SAME_TYPE(typename Vector::reference, decltype(c.back()));
+    ASSERT_SAME_TYPE(typename Vector::const_reference, decltype(cc.back()));
 }
 
 int main(int, char**)
 {
-    {
-        typedef std::vector<int> C;
-        C c = make<C>(10);
-        LIBCPP_ASSERT_NOEXCEPT(c[0]);
-        LIBCPP_ASSERT_NOEXCEPT(c.front());
-        LIBCPP_ASSERT_NOEXCEPT(c.back());
-        // at() is NOT noexcept
-        ASSERT_SAME_TYPE(C::reference, decltype(c[0]));
-        ASSERT_SAME_TYPE(C::reference, decltype(c.at(0)));
-        ASSERT_SAME_TYPE(C::reference, decltype(c.front()));
-        ASSERT_SAME_TYPE(C::reference, decltype(c.back()));
-        for (int i = 0; i < 10; ++i)
-            assert(c[i] == i);
-        for (int i = 0; i < 10; ++i)
-            assert(c.at(i) == i);
-        assert(c.front() == 0);
-        assert(c.back() == 9);
-    }
-    {
-        typedef std::vector<int> C;
-        const int N = 5;
-        const C c = make<C>(10, N);
-        LIBCPP_ASSERT_NOEXCEPT(c[0]);
-        LIBCPP_ASSERT_NOEXCEPT(c.front());
-        LIBCPP_ASSERT_NOEXCEPT(c.back());
-        // at() is NOT noexcept
-        ASSERT_SAME_TYPE(C::const_reference, decltype(c[0]));
-        ASSERT_SAME_TYPE(C::const_reference, decltype(c.at(0)));
-        ASSERT_SAME_TYPE(C::const_reference, decltype(c.front()));
-        ASSERT_SAME_TYPE(C::const_reference, decltype(c.back()));
-        for (int i = 0; i < 10; ++i)
-            assert(c[i] == N + i);
-        for (int i = 0; i < 10; ++i)
-            assert(c.at(i) == N + i);
-        assert(c.front() == N);
-        assert(c.back() == N + 9);
-    }
+    test<std::vector<int>>();
+    test<std::vector<EqComparable>>();
 #if TEST_STD_VER >= 11
-    {
-        typedef std::vector<int, min_allocator<int>> C;
-        const int N = 34;
-        C c = make<C>(10, N);
-        LIBCPP_ASSERT_NOEXCEPT(c[0]);
-        LIBCPP_ASSERT_NOEXCEPT(c.front());
-        LIBCPP_ASSERT_NOEXCEPT(c.back());
-        // at() is NOT noexcept
-        ASSERT_SAME_TYPE(C::reference, decltype(c[0]));
-        ASSERT_SAME_TYPE(C::reference, decltype(c.at(0)));
-        ASSERT_SAME_TYPE(C::reference, decltype(c.front()));
-        ASSERT_SAME_TYPE(C::reference, decltype(c.back()));
-        for (int i = 0; i < 10; ++i)
-            assert(c[i] == N + i);
-        for (int i = 0; i < 10; ++i)
-            assert(c.at(i) == N + i);
-        assert(c.front() == N);
-        assert(c.back() == N + 9);
-    }
-    {
-        typedef std::vector<int, min_allocator<int>> C;
-        const int N = 23;
-        const C c = make<C>(10, N);
-        LIBCPP_ASSERT_NOEXCEPT(c[0]);
-        LIBCPP_ASSERT_NOEXCEPT(c.front());
-        LIBCPP_ASSERT_NOEXCEPT(c.back());
-        // at() is NOT noexcept
-        ASSERT_SAME_TYPE(C::const_reference, decltype(c[0]));
-        ASSERT_SAME_TYPE(C::const_reference, decltype(c.at(0)));
-        ASSERT_SAME_TYPE(C::const_reference, decltype(c.front()));
-        ASSERT_SAME_TYPE(C::const_reference, decltype(c.back()));
-        for (int i = 0; i < 10; ++i)
-            assert(c[i] == N + i);
-        for (int i = 0; i < 10; ++i)
-            assert(c.at(i) == N + i);
-        assert(c.front() == N);
-        assert(c.back() == N + 9);
-    }
+    test<std::vector<int, min_allocator<int>>>();
 #endif
 
   return 0;
